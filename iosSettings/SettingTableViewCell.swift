3576fcdf3567b3bed2255617f0cfe2af32d214b4
GimmyFox <mguzeev99@gmail.com>
2021-12-10 19:45:17 +0300

    setup tableViewCell

---
+++ iosSettings/SettingTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SettingTableViewCell.swift
+//  iosSettings
+//
+//  Created by Maksim Guzeev on 10.12.2021.
+//
+
+import UIKit
+
+class SettingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
