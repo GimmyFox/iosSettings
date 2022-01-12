2ef2dc1410322c47310da8011d372403a14b8195
GimmyFox <mguzeev99@gmail.com>
2022-01-12 23:09:55 +0300

    set vc

--- iosSettings/Sources/Scenes/Controller/ViewController.swift
+++ iosSettings/Sources/Scenes/Controller/ViewController.swift
@@ -9,79 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var models = [SectionSettings]()
+
     
-    private let tableView: UITableView = {
-        let table = UITableView(frame: .zero, style: .grouped)
-        table.register(SettingTableViewCell.self, forCellReuseIdentifier: SettingTableViewCell.identifier)
-        table.register(SwitchTableViewCell.self, forCellReuseIdentifier: SwitchTableViewCell.identifier)
-        
-        return table
-        
-    }()
+    private var settingView: SettingView? {
+        guard isViewLoaded else {return nil}
+        return view as? SettingView
+        }
+    
+    var model: SettingsModel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Настройки"
-        configure()
-        setupHierarchy()
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.frame = view.bounds
-    }
-
-    private func setupHierarchy() {
-        view.addSubview(tableView)
+        view = SettingView()
+        model = SettingsModel()
         
-    }
-    
-    
-}
+        navigationItem.title = "Настройки"
+        navigationController?.navigationBar.prefersLargeTitles = true
 
-extension ViewController: UITableViewDelegate, UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return models[section].options.count
-    }
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return models.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let model = models[indexPath.section].options[indexPath.row]
-        
-        switch model {
-        case .staticCell(let model):
-            guard let cell = tableView.dequeueReusableCell(
-                withIdentifier: SettingTableViewCell.identifier,
-                for: indexPath) as? SettingTableViewCell else {
-                 return UITableViewCell()
-             }
-             
-             cell.configure(with: model)
-             return cell
-            
-        case .switchCell(let model):
-            guard let cell = tableView.dequeueReusableCell(
-                withIdentifier: SwitchTableViewCell.identifier,
-                for: indexPath) as? SwitchTableViewCell else {
-                 return UITableViewCell()
-             }
-             cell.configure(with: model)
-             return cell
+        configureView()
+        }
+
+   }
+
+private extension ViewController {
+    func configureView() {
+        guard let models = model?.createModel() else {return}
+        models.forEach { [unowned self] model in
+            settingView?.configureView(with: models)
         }
     }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        let type = models[indexPath.section].options[indexPath.row]
-        switch type.self {
-        case .staticCell(let model):
-            model.description()
-        case .switchCell(let model):
-            model.description()
-        }
-    }
-    
 }
+
